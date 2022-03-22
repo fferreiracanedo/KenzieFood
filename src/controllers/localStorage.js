041f3765265d29f88bc3aa2ca810adc12d177ecc
Caua Lemos <cauamtm@outlook.com>
2022-03-22 18:46:28 -0300

    feat: adicionando o autenticador na pagina admin

--- src/controllers/localStorage.js
+++ src/controllers/localStorage.js
@@ -2,6 +2,7 @@ export class LocalStorage{
     static addLocalStorage(data){
         localStorage.setItem('key', JSON.stringify(data))
     }
+    
     static getLocalStorageAutenticador(){
         if(localStorage.getItem('key') == undefined){
             window.location.href = './src/pages/register.html'
