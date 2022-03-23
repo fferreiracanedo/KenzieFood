2749ab402d5ef6acdfc62d8af8a56f55e38dd095
Leonardo Rocha <leonard_rock@outlook.com>
2022-03-23 01:04:14 -0300

    feature: modal cadastrar produto agora conecta com a api. Além disso, corrigir algumas outras pendências/inconsistências

--- src/controllers/admin/scriptAdmin.js
+++ src/controllers/admin/scriptAdmin.js
@@ -1,12 +1,22 @@
-import { LocalStorage } from "../localStorage.js";
-import { adminPage }                    from "./cadastrarProduto.js"
+import { LocalStorage }                 from "../localStorage.js";
+import { adminPage }                    from "./cadastrarProduto.js";
 import { cadastrarProdutoModal }        from "./cadastrarProduto.js"
+import { botoesCategoria }              from "./cadastrarProduto.js"
+import { cadastrarProdutoCategorias }   from "./cadastrarProduto.js"
+import { categoriaInedita }             from "./cadastrarProduto.js"
+
 
 LocalStorage.getLocalStorageAutenticadorAdmin()
+adminPage.carregarCategorias("my/products", botoesCategoria, "Nav")
+adminPage.carregarCategorias("products", cadastrarProdutoCategorias, "Cadastro")
+setTimeout(() => adminPage.habilitarSelecaoCategorias(), 2000)
 
 const adicionarProduto                  = document.getElementById("adicionarProduto")
 const span                              = document.getElementsByClassName("close")[0];
+const cadastrarProdutoForm              = document.getElementById("cadastrarProdutoForm");
+const cadastrarProdutoButton            = document.getElementById("cadastrarProdutoButton")
 
+// cadastrarProdutoForm.                   addEventListener("submit", adminPage.cadastrarNovoProduto)
 adicionarProduto.                       addEventListener("click", adminPage.cadastrarProduto)
 span.                                   addEventListener("click", adminPage.fecharModal)
 window.                                 addEventListener("click", function(event) {
@@ -15,4 +25,12 @@ window.                                 addEventListener("click", function(event
     }
 })
 
-adminPage.carregarCategorias("my/products")
+cadastrarProdutoButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    adminPage.cadastrarNovoProduto()
+})
+
+categoriaInedita.addEventListener("keyup", () => {
+    adminPage.categoriaCustomizada  = categoriaInedita.value
+    console.log(adminPage.categoriaCustomizada)
+})
