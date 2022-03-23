9fcf0ba13268890d79e4ae365fc1cc429fc4d50f
luancatarino <luancatarino@gmail.com>
2022-03-23 07:11:22 -0300

    feat: Template Lista Produtos

--- src/controllers/home/apiPublicaItens.js
+++ src/controllers/home/apiPublicaItens.js
@@ -1,26 +1,32 @@
 export class ApiPublica {
     static async buscarProdutosApi() {
+        const vitrineProdutos = document.getElementById("vitrineProdutos");
+
         await fetch("https://kenzie-food-api.herokuapp.com/products")
             .then((res) => res.json())
             .then((data) => {
-                data.forEach(({ nome, preco, categoria, descricao, imagem }) => {
-                    const vitrineProdutos = document.getElementById("vitrineProdutos");
-                    const cardProduto = document.createElement("div");
-                    
-                    cardProduto.classList.add("cardProduto");
-                   
-
-                    cardProduto.innerHTML = `<img src="${imagem}"></img>
-          <p id="produtoCategoria">${categoria}</p>
-          <h2 id="produtoNome">${nome}</h2>
-          <p id="produtoDescricao"> ${descricao}</p>
-          <div id="precoBtn">
-          <h4 id="produtoPreco">R$${preco}</h4>
-          <button id="btnAdicionarCarrinho"></button>
-          </div>
-          `;
-                    vitrineProdutos.appendChild(cardProduto);
+                console.log(data);
+                data.forEach((produto) => {
+                    const template = ApiPublica.template(produto);
+                    vitrineProdutos.appendChild(template);
                 });
             });
+    }
+
+    static template({ nome, preco, categoria, descricao, imagem }) {
+        const cardProduto = document.createElement("div");
+        cardProduto.classList.add("cardProduto");
+
+        cardProduto.innerHTML = `
+            <img src="${imagem}"></img>
+            <p id="produtoCategoria">${categoria}</p>
+            <h2 id="produtoNome">${nome}</h2>
+            <p id="produtoDescricao"> ${descricao}</p>
+            <div id="precoBtn">
+            <h4 id="produtoPreco">R$${preco}</h4>
+            <button id="btnAdicionarCarrinho"></button>
+            </div>
+`;
+        return cardProduto;
     }
 }
