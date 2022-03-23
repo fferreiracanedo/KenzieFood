f165d18a667d10ad29453dc810784c69f6c1db53
luancatarino <luancatarino@gmail.com>
2022-03-23 14:43:12 -0300

    fix: img/ Add Categoria campo pesquisa

--- src/controllers/home/campoPesquisa.js
+++ src/controllers/home/campoPesquisa.js
@@ -1,25 +1,33 @@
-import { ApiPublica} from "./apiPublicaItens.js"
+import { ApiPublica } from "./apiPublicaItens.js";
 
 export class PesquisaDinamica {
-    static arrayProdutos = []
+    static arrayProdutos = [];
 
-     static filtroPesquisa(array, pesquisa) {
-         
-         if(pesquisa.length == 0 ) {
-             ApiPublica.template(array)
-         }
-     
-        array.filter(produto => {
-            const filtroProduto = produto.nome.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
-            const pesquisaFiltro = pesquisa.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
-           if(filtroProduto.includes(pesquisaFiltro)) {
-            this.arrayProdutos.push(produto)
-            console.log(pesquisa)
-           }
-     }) 
-   
-     ApiPublica.template(this.arrayProdutos)
-    this.arrayProdutos = []
-}
-}
+    static filtroPesquisa(array, pesquisa) {
+        if (pesquisa.length == 0) {
+            ApiPublica.template(array);
+        }
 
+        array.filter((produto) => {
+            const filtroCategoria = produto.categoria
+                .toLowerCase()
+                .normalize("NFD")
+                .replace(/[\u0300-\u036f]/g, "");
+            const filtroProduto = produto.nome
+                .toLowerCase()
+                .normalize("NFD")
+                .replace(/[\u0300-\u036f]/g, "");
+            const pesquisaFiltro = pesquisa
+                .toLowerCase()
+                .normalize("NFD")
+                .replace(/[\u0300-\u036f]/g, "");
+            if (filtroProduto.includes(pesquisaFiltro) || filtroCategoria.includes(pesquisaFiltro)) {
+                this.arrayProdutos.push(produto);
+                console.log(pesquisa);
+            }
+        });
+
+        ApiPublica.template(this.arrayProdutos);
+        this.arrayProdutos = [];
+    }
+}
