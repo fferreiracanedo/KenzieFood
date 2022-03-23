81f69e24780c185fb82582948cb297306fec3efb
Leonardo Rocha <leonard_rock@outlook.com>
2022-03-23 10:22:17 -0300

    feature: as categorias agora estão separadas quando a API puxa mais de uma categoria por produto

--- src/controllers/admin/scriptAdmin.js
+++ src/controllers/admin/scriptAdmin.js
@@ -1,19 +1,16 @@
 
-import { LocalStorage } from "../localStorage.js";
-import { AdminAPI } from "./adminAPI.js";
+import { LocalStorage }                 from "../localStorage.js";
+import { AdminAPI }                     from "./adminAPI.js";
 import { adminPage }                    from "./cadastrarProduto.js"
 
 import { cadastrarProdutoModal }        from "./cadastrarProduto.js"
 import { botoesCategoria }              from "./cadastrarProduto.js"
 import { cadastrarProdutoCategorias }   from "./cadastrarProduto.js"
-import { categoriaInedita }             from "./cadastrarProduto.js"
 
 // AUTENTICACAO
 LocalStorage.getLocalStorageAutenticadorAdmin()
 
 adminPage.carregarCategorias("my/products", botoesCategoria, "Nav")
-adminPage.carregarCategorias("products", cadastrarProdutoCategorias, "Cadastro")
-setTimeout(() => adminPage.habilitarSelecaoCategorias(), 2000)
 
 AdminAPI.produtos()
 
@@ -23,21 +20,27 @@ const span                              = document.getElementsByClassName("close
 const cadastrarProdutoForm              = document.getElementById("cadastrarProdutoForm");
 const cadastrarProdutoButton            = document.getElementById("cadastrarProdutoButton")
 
-// cadastrarProdutoForm.                   addEventListener("submit", adminPage.cadastrarNovoProduto)
+cadastrarProdutoButton.                 addEventListener("click", (e)=>{
+    e.preventDefault()
+    adminPage.cadastrarNovoProduto()
+})
 adicionarProduto.                       addEventListener("click", adminPage.cadastrarProduto)
+
+adicionarProduto.addEventListener("click", () => {
+    cadastrarProdutoCategorias.innerHTML=""
+    adminPage.carregarCategorias("my/products", cadastrarProdutoCategorias, "Cadastro")
+    setTimeout(() => adminPage.habilitarSelecaoCategorias(), 700)
+    setTimeout(() => adminPage.gerarCustomizarCategoria(), 700)
+    setTimeout(() => {
+        adminPage.categoriaInedita.addEventListener("keyup", () => {
+            adminPage.categoriaCustomizada  = adminPage.categoriaInedita.value
+            console.log(adminPage.categoriaCustomizada)
+        })
+    }, 700)
+})
 span.                                   addEventListener("click", adminPage.fecharModal)
 window.                                 addEventListener("click", function(event) {
     if (event.target == cadastrarProdutoModal) {
         adminPage.fecharModal();
     }
-})
-
-cadastrarProdutoButton.addEventListener('click', (e) => {
-    e.preventDefault()
-    adminPage.cadastrarNovoProduto()
-})
-
-categoriaInedita.addEventListener("keyup", () => {
-    adminPage.categoriaCustomizada  = categoriaInedita.value
-    console.log(adminPage.categoriaCustomizada)
 })
