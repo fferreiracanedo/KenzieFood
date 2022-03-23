1e6ec105616a1bbcefce35d412eb4f41cd6b71cd
luancatarino <luancatarino@gmail.com>
2022-03-23 11:53:16 -0300

    feat: Filtro pesquisa

--- src/controllers/home/mainPage.js
+++ src/controllers/home/mainPage.js
@@ -2,8 +2,20 @@ import { ApiPublica } from './apiPublicaItens.js'
 import { Autenticador } from './verificadorLogin.js'
 import { adminPage } from "../admin/cadastrarProduto.js"
 import { botoesCategoria } from "../admin/cadastrarProduto.js"
+import { PesquisaDinamica } from "./campoPesquisa.js"
 
 
-ApiPublica.buscarProdutosApi()
 Autenticador.autenticar()
+const arrayProdutos = await ApiPublica.buscarProdutosApi()
+ApiPublica.template(arrayProdutos)
 adminPage.carregarCategorias("products", botoesCategoria, "Nav")
+
+console.log(arrayProdutos)
+
+const campoPesquisa = document.querySelector("#pesquisa")
+
+campoPesquisa.addEventListener("keyup", (e)=> {
+    const infoPesquisa =  e.target.value 
+    PesquisaDinamica.filtroPesquisa(arrayProdutos, infoPesquisa)
+})
+
