5a445f33f8900a206a4c661830544be7ec0ddc5e
Mateus Ferreira Canedo <mateusferreiracanedo@gmail.com>
2022-03-23 22:17:58 -0300

    fix/css

--- src/controllers/registrar/registroAPI.js
+++ src/controllers/registrar/registroAPI.js
@@ -1,39 +1,39 @@
 import { LocalStorage } from "../localStorage.js"
 
-export class RegistroAPI{
+export class RegistroAPI {
     static API_URL = 'https://kenzie-food-api.herokuapp.com/'
-    static async registrar(data){
+    static async registrar(data) {
         fetch(`${this.API_URL}auth/register`, {
             "method": "POST",
             "headers": {
                 "Content-Type": "application/json"
             },
-            "body":JSON.stringify(data)
+            "body": JSON.stringify(data)
         })
-        .then((response) => {
-            if(response.ok == true){
-                window.location.href = "./login.html"
-            }
-        })
+            .then((response) => {
+                if (response.ok == true) {
+                    window.location.href = "./login.html"
+                }
+            })
     }
 
-    static async login(data){
+    static async login(data) {
         const response = await fetch(`${this.API_URL}auth/login`, {
             "method": "POST",
             "headers": {
                 "Content-Type": "application/json"
             },
-            "body":JSON.stringify(data)
+            "body": JSON.stringify(data)
         })
         const responseData = response.json()
-        .then((req)=>{
-            if(req.status == 'Error'){
-                window.location.href = '../pages/register.html'
-            }else if(req){
-                LocalStorage.addLocalStorage(req)
-                window.location.href = '../../index.html'
-            }
-        })
+            .then((req) => {
+                if (req.status == 'Error') {
+                    window.location.href = '../pages/register.html'
+                } else if (req) {
+                    LocalStorage.addLocalStorage(req)
+                    window.location.href = '../../index.html'
+                }
+            })
 
     }
 }
